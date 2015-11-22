7a9a9d930ea5a145fc8b990f6e3a6ca9ea63750e
YaroslavO <jaroslavo@softwareplanetgroup.com>
2015-11-23 00:35:01 +0200

    Add create sprint activity

---
+++ app/src/main/java/com/agile/gdpdp/agile/CreateSprintActivity.java
@@ -0,0 +1,13 @@
+package com.agile.gdpdp.agile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateSprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_sprint);
+    }
+}
