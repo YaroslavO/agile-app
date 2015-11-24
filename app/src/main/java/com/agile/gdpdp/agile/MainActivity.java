9ec18722ee38f0de8fe6828d883225389606e22e
YaroslavO <jaroslavo@softwareplanetgroup.com>
2015-11-25 01:25:41 +0200

    Insert 'Add sprint' Button

---
+++ app/src/main/java/com/agile/gdpdp/agile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.agile.gdpdp.agile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
