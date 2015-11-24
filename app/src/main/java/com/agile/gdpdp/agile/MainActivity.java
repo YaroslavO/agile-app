54688f6bb2389e4280a0f5e5480592bf6f6e8c17
YaroslavO <jaroslavo@softwareplanetgroup.com>
2015-11-25 01:37:23 +0200

    Create listener for 'Create sprint button'

--- app/src/main/java/com/agile/gdpdp/agile/MainActivity.java
+++ app/src/main/java/com/agile/gdpdp/agile/MainActivity.java
@@ -2,12 +2,31 @@ package com.agile.gdpdp.agile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton createSprintButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+        createSprintButton = (ImageButton) findViewById(R.id.createSprintButton);
+
+        createSprintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "CreateSprintButton is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
